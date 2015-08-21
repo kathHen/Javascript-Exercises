944c941cbaa0bf2bcdebdaf5d34c03e3b87da865
kathHen <kath.henderson@tombola.com>
2015-08-21 16:13:17 +0100

    esnkljgdrkljgadklr

--- degreesToRadians.js
+++ degreesToRadians.js
@@ -1,4 +1,4 @@
 'use strict';
 var degree =30;
-degree = ((degree * Math.PI) / 180);
-console.log("You have " + degree + " radians")
+var radians = ((degree * Math.PI) / 180);
+console.log("You have " + radians + " radians")
