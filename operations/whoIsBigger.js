841bd1d9afb1aa4aa12807f3e3e36fef308284d0
kathHen <kath.henderson@tombola.com>
2015-08-21 16:46:36 +0100

    Migrating OPs work

---
+++ operations/whoIsBigger.js
@@ -0,0 +1,10 @@
+'use strict';
+var value1 = 15;
+var value2 = 24;
+if (value1 > value2){
+    console.log("Value 1 is larger than Value 2");
+} else if (value1 === value2) {
+    console.log("The values are equal");
+} else {
+    console.log("Value 2 is bigger than Value 1");
+};
