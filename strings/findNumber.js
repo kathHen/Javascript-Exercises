33b91ba0429edb404222e046061f143bd123fbc0
kathHen <kath.henderson@tombola.com>
2015-09-02 12:08:21 +0100

    Tried to change the regex again, actually accepts decimals now

--- strings/findNumber.js
+++ strings/findNumber.js
@@ -1,6 +1,6 @@
 'option explicit';
 var randomWord = "1a";
-if (randomWord.match("^\\d+$")) {
+if (randomWord.match("\\d+(\\.\\d+)*")) {
     console.log("It's a number!");
 } else  {
     console.log("It's not a number!");
