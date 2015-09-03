8da6a5acc23d7b530b921ffdbf9e05c4e956031b
kathHen <kath.henderson@tombola.com>
2015-09-03 09:57:22 +0100

    Update bythree.js
    Changed the function so that the numbers in the array are only multiples of 3 (rather than having two arrays)

--- arrays/bythree.js
+++ arrays/bythree.js
@@ -1,15 +1,11 @@
 'use strict';
-var theArray = [];
 var findingthrees = function() {
-    for (var i = 0; i < 50; i++) {
-        theArray [i] = i + 1;
+    var threesArray = [];
+    for (var i = 1; i < 50; i++) {
+        if (i%3 === 0){
+            threesArray.push(i);
+         }
     }
-    ;
-    for (var i = 0; i < theArray.length; i++) {
-        if (theArray[i] % 3 === 0) {
-            console.log(theArray[i]);
-        }
-    }
-    ;
+    return threesArray;
 };
 findingthrees();
